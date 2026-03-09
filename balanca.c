e3519cba5a9246777dbd5cd29b10f81fcd9b7f1f
Gersi Uta <75692920+Gersi266@users.noreply.github.com>
2026-03-09 11:48:24 +0100

    Modify balance calculation and output format

--- balanca.c
+++ balanca.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <omp.h>
 int main() {
-    int x = 10;
-    int d[10] = {100, 50, 200, 400};
+    int x = 4;
+    int d[] = {100, 50, 200, 400};
     int balance = 1000;
     #pragma omp parallel for reduction(+:balance)
     for (int i = 0; i < x; i++) {
+        int tid = omp_get_thread_num();
+        printf("Thread %d: balance perpara = %d, shtojme d[%d] = %d\n",
+               tid, balance, i, d[i]);
         balance += d[i];
+        printf("Thread %d: balance perfundimtare = %d\n", tid, balance);
     }
-    printf("Balance totale: %d\n", balance);
+    printf("Total balance: %d\n", balance);
     return 0;
 }
